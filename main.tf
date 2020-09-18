5b1304f7b091bb248e286399086015377876f93f
Gabor Balogh <gabor.balogh1@gmail.com>
2020-09-18 18:58:46 +0100

    Added login for azure with variables
    line 14-18

--- main.tf
+++ main.tf
@@ -9,6 +9,13 @@ terraform {
 
 provider "azurerm" {
   version = "=2.20.0"
+  
+  ## add tenant id with variables
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+  client_secret   = var.client_secret
+  client_id       = var.client_id
+  
   features {}
 }
 
