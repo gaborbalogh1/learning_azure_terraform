2fcb87e8b59088f824b976d74e9a4717593953ff
Gabor Balogh <gabor.balogh1@gmail.com>
2020-09-19 15:47:59 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ resource "azurerm_subnet" "subnet" {
   name                 = "${var.prefix}-subnet"
   virtual_network_name = azurerm_virtual_network.vnet.name
   resource_group_name  = azurerm_resource_group.TerraformAzure.name
-  address_prefixes       = var.subnet_prefix
+  address_prefix       = var.subnet_prefix
 }
 
 resource "azurerm_network_security_group" "tf-sg" {
